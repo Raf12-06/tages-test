849c7cd07b4e6be76387ddf4f1bca3611a6ec57a
Rafail <myhametshin.rafael@outlook.com>
2023-07-08 02:58:41 +0400

    init

--- src/sort.ts
+++ src/sort.ts
@@ -1,37 +1,87 @@
-import { statSync, createReadStream } from 'node:fs'
+import { statSync, createReadStream, createWriteStream } from 'node:fs'
 
 const stat = statSync('text.txt')
 if (!stat.isFile()) {
   process.exit(1)
 }
 
-const FILE_SIZE = stat.size
-const PART_BYTE = 1_000_000 //"524_288_000"
-let NUM_PART = Math.ceil(Number(FILE_SIZE) / PART_BYTE)
-let OFFSET_BYTE = 0
+const PART_BYTE = 524_288_000
 
+const listFile = ['text.txt', 'text2.txt']
+
+let wasChange = false
 async function processFile() {
-  while (NUM_PART) {
+
+  for (let i = 0; i < 10000; i++) {
     await new Promise((res, rej) => {
-      const readStream = createReadStream('text.txt', {
+      const writeStream = createWriteStream(listFile[1])
+      const readStream = createReadStream(listFile[0], {
         encoding: 'utf-8',
-        start: OFFSET_BYTE,
-        end: OFFSET_BYTE + PART_BYTE,
+        highWaterMark: PART_BYTE
       })
 
-      readStream.on('data', data => {
-
+      readStream.on('data', (chunk: string) => {
+        const rowPart = normalizeChunk(chunk).split('\n')
+        const rows = compareRows(rowPart);
+        writeStream.write(rows);
       })
 
       readStream.on('close', () => {
-        NUM_PART--
-        OFFSET_BYTE += PART_BYTE
+        writeStream.close()
+        res(null)
       })
 
       readStream.on('error', err => {
-        console.log(err);
+        writeStream.close()
+        rej(err);
       })
     })
+
+    listFile.reverse();
   }
 }
 
+let lostRow = ''
+function normalizeChunk(chunk: string) {
+  if (lostRow) chunk = lostRow + chunk
+
+  const lastIndex = chunk.length - 1
+  const lastSymbol = chunk[lastIndex]
+  if (lastSymbol !== '\n') {
+    let endIndex = 0
+    for (let i = lastIndex; i >= 0; i--) {
+      const symbol = chunk[i]
+      if (symbol !== '\n') {
+        endIndex = i
+      } else break
+    }
+
+    lostRow = chunk.substring(endIndex)
+    return chunk.substring(0, endIndex)
+  } else {
+    return chunk;
+  }
+}
+
+let lastStr = '';
+function compareRows(rows: string[]): string {
+  let resultStr = '';
+  for (let i = 0; i < rows.length; i++) {
+    const str = rows[i]
+    if (str) {
+      if (!lastStr) lastStr = str
+      else {
+        if (str > lastStr) {
+          wasChange = true
+          resultStr += lastStr + '\n'
+          lastStr = str
+        } else {
+          resultStr += str + '\n'
+        }
+      }
+    }
+  }
+  return resultStr;
+}
+
+processFile().catch(e => console.log(e));
